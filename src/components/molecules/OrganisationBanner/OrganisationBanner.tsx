9b61ffcd291bdca4ab4a1230849e470a095b65be
Braden Marshall <braden1996@hotmail.co.uk>
2019-12-24 11:45:30 +0000

    :iphone: Add mobile support for OrganisationBanner.

--- src/components/molecules/OrganisationBanner/OrganisationBanner.tsx
+++ src/components/molecules/OrganisationBanner/OrganisationBanner.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { DateRange } from '@atoms';
-import styled from '@styled';
+import styled, { media } from '@styled';
 
 const ImageContainer = styled.span`
   display: flex;
@@ -20,20 +20,29 @@ const ImageContainer = styled.span`
   }
 `;
 const Name = styled.span``;
-const Role = styled.span``;
-const Location = styled.span``;
+const Role = styled.span`
+  font-variant-caps: small-caps;
+  color: ${p => p.theme.colors.use.text.secondary};
+`;
+const Location = styled.span`
+  color: ${p => p.theme.colors.use.accent.secondary};
+`;
 const StyledDateRange = styled(DateRange)``;
 
 const Container = styled.section`
   display: grid;
-  grid-template-columns: min-content repeat(2, 1fr);
+  grid-template-columns: min-content 1fr;
   grid-column-gap: ${p => p.theme.dimensions.use.margin};
-  grid-template-rows: repeat(2, 1fr);
+  grid-row-gap: ${p => p.theme.dimensions.base.xSmall};
+  grid-template-rows: repeat(4, 1fr);
+
+  & > ${ImageContainer}, & > ${Name}, & > ${Role}, & > ${Location}, & > ${StyledDateRange} {
+    justify-self: flex-start;
+    align-self: center;
+  }
 
   & > ${ImageContainer} {
-    grid-area: 1 / 1 / 3 / 1;
-    justify-self: center;
-    align-self: center;
+    grid-area: 1 / 1 / 5 / 1;
   }
 
   & > ${Name} {
@@ -42,22 +51,65 @@ const Container = styled.section`
 
   & > ${Role} {
     grid-area: 2 / 2;
-    align-self: flex-end;
-    font-variant-caps: small-caps;
-    color: ${p => p.theme.colors.use.text.secondary};
   }
 
   & > ${Location} {
-    grid-area: 1 / 3;
-    justify-self: flex-end;
-    color: ${p => p.theme.colors.use.accent.secondary};
+    grid-area: 3 / 2;
   }
 
   & > ${StyledDateRange} {
-    grid-area: 2 / 3;
-    justify-self: flex-end;
-    align-self: flex-end;
+    grid-area: 4 / 2;
   }
+
+  ${media.greaterThan('mobile')`
+    grid-template-columns: min-content repeat(2, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+
+    & > ${Name} {
+      grid-column-end: 4;
+    }
+
+    & > ${Role} {
+      grid-column-end: 4;
+    }
+
+    & > ${Location} {
+      grid-column-end: 3;
+    }
+
+    & > ${StyledDateRange} {
+      grid-area: 3 / 2 / 3 / 4;
+      justify-self: flex-end;
+      align-self: flex-end;
+    }
+  `}
+
+  ${media.greaterThan('tablet')`
+    grid-template-rows: repeat(2, 1fr);
+
+    & > ${ImageContainer} {
+      grid-area: 1 / 1 / 3 / 1;
+    }
+
+    & > ${Name} {
+      grid-area: 1 / 2;
+    }
+
+    & > ${Role} {
+      align-self: flex-end;
+    }
+
+    & > ${Location} {
+      grid-area: 1 / 3;
+      justify-self: flex-end;
+    }
+
+    & > ${StyledDateRange} {
+      grid-area: 2 / 3;
+      justify-self: flex-end;
+      align-self: flex-end;
+    }
+  `}
 `;
 
 interface Props {
